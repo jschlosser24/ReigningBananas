d885e0d9e551c0fc8e643d4e9469a0d3306432c5
jschlosser24 <jschlosser24@gmail.com>
2017-03-19 22:13:51 -0500

    Fixed it

--- homepage.js
+++
@@ -1,9 +0,0 @@
-function getName(){
-  var currentUser = Parse.User.current();
-  if (currentUser.get("name") != null) {
-    document.getElementById('nameText').innerHTML = currentUser.get("name") + "!";
-  } else {
-    document.getElementById('nameText').innerHTML = currentUser.getUsername() + "!";
-  }
-}
-window.onload = getName;
