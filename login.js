cfcfb1e4955d452676c9e8b2d806aceab3c0ebec
jschlosser24 <jschlosser24@gmail.com>
2017-03-21 22:32:13 -0500

    formatting

--- login.js
+++ login.js
@@ -1,7 +1,4 @@
 function login(username, password){
-  var usernameText = document.getElementById('usernameText');
-  var passwordText = document.getElementById('passwordText');
-
   Parse.User.logIn(username, password, {
     success: function(user) {
       window.location.href = 'homepage.html';
--- test/loginTests.js
+++ test/loginTests.js
@@ -1,7 +1,20 @@
 $(document).ready(function () {
 
   QUnit.test("login", function(assert) {
+    assert.expect(1);
     login("user4", "pass4")
-    assert.equal(window.location.href, "homepage.html", "login goes to homepage with accurate user")
+    assert.equal(window.location.href, "../homepage.html", "login goes to homepage with accurate user")
   });
+
+  // QUnit.test("homepage", function(assert) {
+  //   assert.expect(2);
+  //
+  //   var nameText = $("div[id$=nameText]");
+  //   var name = "Kevin";
+  //
+  //   assert.ok(nameText, "nameText input exists and made it to the homepage");
+  //
+  //   assert.equal(getName(), name, "The logged in user should have the name kevin");
+  //
+  // });
 });
