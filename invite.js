99c9e2900511a59845149b25d8794a09368af6b2
jschlosser24 <jschlosser24@gmail.com>
2017-03-26 20:48:29 -0500

    invite start

---
+++ invite.js
@@ -0,0 +1,27 @@
+function invite(username, projectName) {
+  if (username == null || projectName == null){
+    alert("Please fill out all fields before submitting.");
+  }
+  var user = Parse.Object.extend("User");
+  var project = Parse.Object.extend("Project");
+  var query = new Parse.Query("User");
+  query.equalTo("username", username);
+  query.get({
+    success: function(user) {
+      var query2 = new Parse.Query("Project");
+      query.equalTo("name", projectName);
+      query.get({
+        success: function(project) {
+          //add user to project
+          window.location.href = "invite.html"
+        },
+        error: function(projectName) {
+          alert("Error: Could not find project '" + projectName + "' in database");
+        }
+      });
+    },
+    error: function(username) {
+      alert("Error: Could not find user '" + username + "' in database.");
+    }
+  });
+}
