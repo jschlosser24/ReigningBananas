feb656c51ad612e8e39e989e361b0b11eb4dbbea
jtobert <jtobert@csbsju.edu>
2017-03-27 17:39:51 -0500

    Added Create Project to the Homepage and need JS

---
+++ newProject.js
@@ -0,0 +1,18 @@
+function newProject(newProjectName){
+  
+  Parse.User.projects.add(newProjectName, {
+    success: function(user) {
+      window.location.href = "homepage.html";
+    },
+    error: function(user, error) {
+      alert("Error: " + error.code + " " + error.message);
+    }
+});
+}
+
+/**Parse.Object current( )
+Retrieves the currently logged in ParseUser with a valid session, either from memory or localStorage, if necessary.
+
+
+Returns: <Parse.Object>
+The currently logged in Parse.User.**/
