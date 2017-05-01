7ccbc698744bd04a212a6a134da49a9f0cbcfe8a
ndmobilecomplab <connororourke25@gmail.com>
2017-04-30 20:44:58 -0500

    commit

--- addingPBL.js
+++ addingPBL.js
@@ -1,3 +1,5 @@
+var projectID = getVar(window.location.href);
+
 function addingPBL(storyDescription, role, functionality, val, acceptanceCriteria, size){
   var user = Parse.User.current();
 
@@ -15,8 +17,27 @@ function addingPBL(storyDescription, role, functionality, val, acceptanceCriteri
       pbl.set("acceptanceCriteria", acceptance);
       pbl.set("size", size);
       pbl.set("addedBy", user.id);
+      pbl.set("projectId", projectID);
+      pbl.set("column", 0);
+      pbl.set("row", 0);
+      var id = pbl.id;
+      var query = new Parse.Query("ScrumBoardItems");
+      query.equalTo("projectId", projectID).notEqualTo("objectId", id);
+      query.find({
+        success: function(items) {
+          for (var i = 0; i < items.length; i++){
+            var row = items[i].get("row");
+            items[i].set("row", row + 1);
+            items.save();
+          }
+        }
     }, error: function(error) {
       alert("Error: " + error.code + " " + error.message);
     }
   });
+}
+
+function getVar(str) {
+  var point = str.lastIndexOf("=");
+  return str.substring(point+1,str.length);
 }
